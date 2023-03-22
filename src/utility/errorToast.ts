eb2dc40d660b85ac1ef240e88c0e73966cdb814a
Your Name <you@example.com>
2023-03-22 14:57:22 -0400

    move toast to bottom left

--- src/utility/errorToast.ts
+++ src/utility/errorToast.ts
@@ -2,7 +2,7 @@ import { toast } from "react-toastify";
 
 export function showErrorToast(message: string) {
   toast.error(message, {
-    position: "bottom-right",
+    position: "bottom-left",
     autoClose: 5000,
     hideProgressBar: false,
     closeOnClick: true,
--- src/utility/successToast.ts
+++ src/utility/successToast.ts
@@ -2,7 +2,7 @@ import { toast } from "react-toastify";
 
 export function showSuccessToast(message: string) {
   toast.success(message, {
-    position: "bottom-right",
+    position: "bottom-left",
     autoClose: 5000,
     hideProgressBar: false,
     closeOnClick: true,
