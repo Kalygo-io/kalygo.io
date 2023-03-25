7c32b681048875acc8f639acd66b413574b3bbac
Your Name <you@example.com>
2023-03-25 15:30:06 -0400

    add Sentry.io

--- src/index.tsx
+++ src/index.tsx
@@ -1,30 +1,30 @@
-import React from "react";
 import ReactDOM from "react-dom/client";
-
 import "./scss/volt.scss";
 import "react-datetime/css/react-datetime.css";
 import "react-toastify/dist/ReactToastify.css";
-
 import { HashRouter } from "react-router-dom";
-
 import { store, persistor } from "./store/store";
 import { Provider } from "react-redux";
-
 import ScrollToTop from "./components/ScrollToTop";
-
 import reportWebVitals from "./reportWebVitals";
 import Router from "./pages/Router";
 import { PersistGate } from "redux-persist/integration/react";
-
 import package_json from "../package.json";
 
-console.log("package_json.resetRedux", package_json.resetRedux);
+import * as Sentry from "@sentry/react";
+import { BrowserTracing } from "@sentry/tracing";
+
+// console.log("package_json.resetRedux", package_json.resetRedux);
+Sentry.init({
+  dsn: "https://92d5188348a24f92bb7b85b7d0ff62dd@o4504900846813184.ingest.sentry.io/4504900846878720",
+  integrations: [new BrowserTracing()],
+  tracesSampleRate: 1.0,
+});
 
 if (localStorage.getItem("clearReduxPersist") !== package_json.resetRedux) {
   persistor.purge();
   localStorage.setItem("clearReduxPersist", package_json.resetRedux);
 }
-
 const root = ReactDOM.createRoot(
   document.getElementById("root") as HTMLElement
 );
