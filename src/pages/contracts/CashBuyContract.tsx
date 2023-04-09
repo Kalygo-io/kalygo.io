5e19cad242560e9d2c4e7647f66daecb485a83ac
a <a@a.com>
2023-04-09 03:41:20 -0400

    edit 'Cash Buy' to say 'Escrow'

--- src/pages/contracts/CashBuyContract.tsx
+++ src/pages/contracts/CashBuyContract.tsx
@@ -30,7 +30,7 @@ const CashBuyContract = () => {
   return (
     <>
       <div className="d-flex justify-content-between flex-wrap flex-md-nowrap align-items-center py-4">
-        <h1>Cash Buy</h1>
+        <h1>Escrow</h1>
       </div>
 
       <Row>
