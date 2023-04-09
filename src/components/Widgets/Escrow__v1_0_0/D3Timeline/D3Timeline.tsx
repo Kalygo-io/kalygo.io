bc5f59e2d8248e51b3417c2515d58604ed26ea29
a <a@a.com>
2023-04-09 15:50:51 -0400

    setup and test requestAnimationFrame for animating current time on timeline

--- src/components/Widgets/Escrow__v1_0_0/D3Timeline/D3Timeline.tsx
+++ src/components/Widgets/Escrow__v1_0_0/D3Timeline/D3Timeline.tsx
@@ -16,6 +16,16 @@ export const D3Timeline = (props: P) => {
 
   const rootRef = useRef<HTMLDivElement | null>(null);
   const size = useResize(rootRef);
+  // related to animation
+  const animationRef = useRef<{
+    requestId: number | null;
+    previousTime: number | null;
+    elapsedFrame: number;
+  }>({
+    requestId: null,
+    previousTime: null,
+    elapsedFrame: 0,
+  });
 
   useEffect(() => {
     select(rootRef.current)?.select("svg").remove();
@@ -30,6 +40,31 @@ export const D3Timeline = (props: P) => {
       drawStaticElements(rootRef.current!, size, events, now);
     }
   }, [size]);
+
+  useEffect(() => {
+    animationRef.current.requestId = requestAnimationFrame(animate);
+    return () => cancelAnimationFrame(animationRef.current.requestId!);
+  }, [size]); // Make sure the effect runs only once
+
+  const animate = (time: any) => {
+    // console.log("animate");
+    const FRAME_RATE = 1000 / 4; // in milliseconds ie : 2 is the amount of frames/sec
+    const now = Date.now();
+    if (animationRef.current.previousTime !== undefined) {
+      const deltaTime = time - animationRef.current.previousTime!;
+      animationRef.current.elapsedFrame! += deltaTime;
+      if (
+        animationRef.current.elapsedFrame > FRAME_RATE &&
+        // now < events[events.length - 1].ts &&
+        size
+      ) {
+        console.log("DRAW");
+        animationRef.current.elapsedFrame = 0;
+      }
+    }
+    animationRef.current.previousTime = time;
+    animationRef.current.requestId = requestAnimationFrame(animate);
+  };
 
   return (
     <div ref={rootRef} id="d3-timeline-widget">
